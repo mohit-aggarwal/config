fa778297c7e5caada5a3e9a8d652dd68575178e0
mohit-aggarwal <mohit-aggarwal@users.noreply.github.com>
2017-01-13 17:29:28 +0530

    added installation script

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+# Install vimrc
+git clone git://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
+
+
+# Install bashrc
+git clone git://gitbub.com/mohit-aggarwal/config.git /tmp/config
+cp /tmp/config/.bashrc ~/.bashrc
+
